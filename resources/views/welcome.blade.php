439f1315f80b072bfbaf8f28da1197cd49def2eb
Wasan Ongart <o_wasan@hotmail.com>
2020-05-01 22:59:30 +0700

    update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,19 @@
 
 <!--image-->
 
-<div class="max-w-full mx-auto bg-gray-300 bg-cover pt-16 "
+<div class="max-w-full mx-auto bg-gray-300 bg-cover pt-12 "
     style="background-image:url('{{  Storage::url('mshome3.jpg') }}'); ">
 
     <div class="p-6">
         <div class="xl:flex -mx-6">
-            <div class="max-w-2xl mx-auto p-4 md:p-4 t leading-normal ">
+            <div class="max-w-2xl mx-auto p-4 md:p-4 leading-tight">
 
                 <h1 class="text-3xl text-black font-extrabold sm:text-4xl md:text-5xl xl:text-4xl leading-tight">
                     Classifieds website portal
                 </h1>
                 <span class="sm:block text-indigo-500 text-2xl font-bold ">
                     โพสโฆษณาธุรกิจตามประเภทที่ต้องการ</span>
-                <p class="mt-4 leading-relaxed sm:text-base md:text-xl xl:text-lg text-gray-900">
+                <p class="mt-2 leading-relaxed sm:text-base md:text-xl xl:text-lg text-black antialiased ">
                     แพลตฟอร์มออนไลน์ง่ายๆ ที่ผู้ใช้สามารถโพสต์หรือดูโฆษณาได้ตามความต้องการ
                     โพสโฆษณาใน MStore มีการแยกประเภทและหมวดหมู่ย่อยที่แตกต่างกันเพื่อให้ลูกค้าสามารถ
                     ได้รับข้อมูลที่ต้องการได้อย่างรวดเร็ว
