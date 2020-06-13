b11d9c6faf02a78f491076617471047b8d8ca9f1
Wasan Ongart <o_wasan@hotmail.com>
2020-06-13 22:38:35 +0700

    field js thaiaddress

--- nova-components/InputThaiAddress/resources/js/field.js
+++ nova-components/InputThaiAddress/resources/js/field.js
@@ -4,24 +4,24 @@ Nova.booting((Vue, router) => {
     //add address filed 4 sub-district,districe,province,postal-code
 
     //sub-district
-    Vue.component('index-input-sub-district', require('./components/InputSubDistrict/IndexField'))
-    Vue.component('detail-input-sub-district', require('./components/InputSubDistrict/DetailField'))
-    Vue.component('form-input-sub-district', require('./components/InputSubDistrict/FormField'))
+    Vue.component('index-input-sub-district', require('./components/InputSubDistrict/IndexField').default);
+    Vue.component('detail-input-sub-district', require('./components/InputSubDistrict/DetailField').default);
+    Vue.component('form-input-sub-district', require('./components/InputSubDistrict/FormField').default);
 
     //district
-    Vue.component('index-input-district', require('./components/InputDistrict/IndexField'))
-    Vue.component('detail-input-district', require('./components/InputDistrict/DetailField'))
-    Vue.component('form-input-district', require('./components/InputDistrict/FormField'))
+    Vue.component('index-input-district', require('./components/InputDistrict/IndexField').default);
+    Vue.component('detail-input-district', require('./components/InputDistrict/DetailField').default);
+    Vue.component('form-input-district', require('./components/InputDistrict/FormField').default);
 
     //province
-    Vue.component('index-input-province', require('./components/InputProvince/IndexField'))
-    Vue.component('detail-input-province', require('./components/InputProvince/DetailField'))
-    Vue.component('form-input-province', require('./components/InputProvince/FormField'))
+    Vue.component('index-input-province', require('./components/InputProvince/IndexField').default);
+    Vue.component('detail-input-province', require('./components/InputProvince/DetailField').default);
+    Vue.component('form-input-province', require('./components/InputProvince/FormField').default);
 
     //postal-code
-    Vue.component('index-input-postal-code', require('./components/InputPostalCode/IndexField'))
-    Vue.component('detail-input-postal-code', require('./components/InputPostalCode/DetailField'))
-    Vue.component('form-input-postal-code', require('./components/InputPostalCode/FormField'))
+    Vue.component('index-input-postal-code', require('./components/InputPostalCode/IndexField').default);
+    Vue.component('detail-input-postal-code', require('./components/InputPostalCode/DetailField').default);
+    Vue.component('form-input-postal-code', require('./components/InputPostalCode/FormField').default);
 
 
 
