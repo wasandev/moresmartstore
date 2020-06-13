a76657c031a74bf84022f3d9c0b7838f3aa5aaac
Wasan Ongart <o_wasan@hotmail.com>
2020-06-13 22:03:22 +0700

    update thai address input

--- resources/views/partials/businesstype.blade.php
+++ resources/views/partials/businesstype.blade.php
@@ -3,7 +3,7 @@
     @include('partials.headbar',[
         'svg' => '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="fill-current absolute  "><path d="M1 4h2v2H1V4zm4 0h14v2H5V4zM1 9h2v2H1V9zm4 0h14v2H5V9zm-4 5h2v2H1v-2zm4 0h14v2H5v-2z"/></svg>',
         'title' => 'ประเภทธุรกิจ',
-        'link' => '/vendor',
+        'link' => '/vendors',
         'linktext' => 'แสดงทั้งหมด'
     ])
     <div class="w-full xl:mx-0 rounded-lg">
