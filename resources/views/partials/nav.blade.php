d2eb1453f25cccdee430d5d06ee646171763c3f1
Wasan Ongart <o_wasan@hotmail.com>
2020-07-06 21:15:48 +0700

    update nav for edge

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -37,29 +37,29 @@
         <div id="main-nav" class="lg:block lg:w-auto hidden text-base font-medium lg:bg-white bg-gray-200 text-center">
             <div class="lg:flex lg:justify-between items-center">
                 <a href="/" class="mx-4 block px-4 py-2 text-gray-800  rounded-full bg-gray-200 hover:bg-blue-700 hover:text-gray-100">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"/>
-                    </svg>
-                    <span class="ml-6">หน้าหลัก</span>
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"/>
+                    </svg> --}}
+                    <span>หน้าหลัก</span>
                 </a>
                 <a href="/vendors" class="mx-4 block px-4 py-2 text-gray-800  rounded-full bg-gray-200 hover:bg-blue-700 hover:text-gray-100  ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M2 2c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v18l-8-4-8 4V2z"/>
-                    </svg>
-                    <span class="ml-6">ข้อมูลธุรกิจ</span>
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M2 2c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v18l-8-4-8 4V2z"/>
+                    </svg> --}}
+                    <span>ข้อมูลธุรกิจ</span>
                 </a>
                 <a href="/products" class="mx-4 block px-4 py-2 text-gray-800  rounded-full bg-gray-200 hover:bg-blue-700 hover:text-gray-100 ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="24" height="24" viewBox="0 0 24 24"><path class="heroicon-ui" d="M5 3h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z"/>
-                    </svg>
-                    <span class="ml-6">สินค้า/บริการ</span>
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="24" height="24" viewBox="0 0 24 24"><path class="heroicon-ui" d="M5 3h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z"/>
+                    </svg> --}}
+                    <span>สินค้า/บริการ</span>
                 </a>
                 <a href="/post" class="mx-4 block px-4 py-2 text-gray-800  rounded-full bg-gray-200 hover:bg-blue-700 hover:text-gray-100 ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="24" height="24" viewBox="0 0 24 24"><path class="heroicon-ui" d="M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"/>
-                    </svg>
-                    <span class="ml-6">โพส</span>
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="24" height="24" viewBox="0 0 24 24"><path class="heroicon-ui" d="M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"/>
+                    </svg> --}}
+                    <span>โพส</span>
                 </a>
                 <a href="/blogs" class="mx-4 block px-4 py-2 text-gray-800  rounded-full bg-gray-200 hover:bg-blue-700 hover:text-gray-100  ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M16 2h4v15a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V0h16v2zm0 2v13a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4h-2zM2 2v15a1 1 0 0 0 1 1h11.17a2.98 2.98 0 0 1-.17-1V2H2zm2 8h8v2H4v-2zm0 4h8v2H4v-2zM4 4h8v4H4V4z"/>
-                    </svg>
-                    <span class="ml-6">บทความ</span>
+                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M16 2h4v15a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V0h16v2zm0 2v13a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4h-2zM2 2v15a1 1 0 0 0 1 1h11.17a2.98 2.98 0 0 1-.17-1V2H2zm2 8h8v2H4v-2zm0 4h8v2H4v-2zM4 4h8v4H4V4z"/>
+                    </svg> --}}
+                    <span>บทความ</span>
                 </a>
 
                 @auth
@@ -98,9 +98,9 @@
                 @else
                     <a href="{{ route('login') }}"
                         class=" mx-4 block px-4 py-2 text-red-600 border-2 border-red-500 rounded-full hover:bg-red-500 hover:text-gray-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/></svg>
+                        {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-4"  width="20" height="20" viewBox="0 0 20 20"><path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z"/></svg> --}}
 
-                        <span class="ml-6">{{ __('Login') }}</span>
+                        <span>{{ __('Login') }}</span>
                     </a>
                 @endauth
 
