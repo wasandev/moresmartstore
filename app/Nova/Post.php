3c9a461d2767fc51e3c080b875bbc0c060b0758a
Wasan Ongart <o_wasan@hotmail.com>
2020-08-03 16:42:03 +0700

    post nova

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -94,7 +94,7 @@ class Post extends Resource
             Image::make(__('Image'),  'post_image')
                 ->hideFromIndex()
                 ->maxWidth(600)
-                ->rules('required','dimensions:max_width=2400,max_height=1260','image', 'max:1024')
+                ->rules('dimensions:max_width=2400,max_height=1260','image', 'max:1024')
                 ->help('ขนาดรูปภาพที่เหมาะสมไม่เกิน 2400x1260px และขนาดไฟล์ไม่เกิน 1 Mb.'),
 
             DateTime::make(__('Published_at'),  'published_at')
