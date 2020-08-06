365f1f81643597bfc08c5cdd2acb8c81c3027eaa
Wasan Ongart <o_wasan@hotmail.com>
2020-08-06 16:48:33 +0700

    update blogs

--- resources/views/partials/blogsearch.blade.php
+++ resources/views/partials/blogsearch.blade.php
@@ -5,7 +5,7 @@
         <div class="max-w-md mx-auto">
             <div class="flex w-full p-4 md:p-4">
                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="search" role="presentation" class="fill-current absolute  ml-3 mt-3 text-gray_200"><path fill-rule="nonzero" d="M14.32 12.906l5.387 5.387a1 1 0 0 1-1.414 1.414l-5.387-5.387a8 8 0 1 1 1.414-1.414zM8 14A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"></path></svg>
-                <input name="blog-search" type="search" placeholder="ค้นหาบทความ" class="transition-colors duration-100 ease-in-out focus:outline-none focus:shadow-md border border-transparent focus:bg-white  placeholder-gray-600 rounded-lg bg-gray-100 py-2 pr-4 pl-10 block w-full appearance-none leading-normal ">
+                <input name="blog-search" type="search" placeholder="ค้นหาข่าว/บทความ" class="transition-colors duration-100 ease-in-out focus:outline-none focus:shadow-md border border-transparent focus:bg-white  placeholder-gray-600 rounded-lg bg-gray-100 py-2 pr-4 pl-10 block w-full appearance-none leading-normal ">
 
             </div>
         </div>
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -62,7 +62,7 @@
                 <a href="/blogs" class="mx-2 block px-3 py-1 text-gray-800 mt-2 lg:mt-0 rounded-full border border-gray-200 hover:bg-blue-500 hover:text-gray-100  ">
                     <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M16 2h4v15a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V0h16v2zm0 2v13a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4h-2zM2 2v15a1 1 0 0 0 1 1h11.17a2.98 2.98 0 0 1-.17-1V2H2zm2 8h8v2H4v-2zm0 4h8v2H4v-2zM4 4h8v4H4V4z"/>
                     </svg>
-                    <span class="ml-6">บทความ</span>
+                    <span class="ml-6">ข่าว/บทความ</span>
                 </a>
 
                 <a href="/pages" class="mx-2 px-3 py-1 block text-red-500 mt-2 lg:mt-0 rounded-full border border-gray-200 hover:bg-blue-500 hover:text-gray-100  ">
