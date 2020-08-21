61fd1b3ea8e95bd9defc324f824bb1aa64916e0c
Wasan Ongart <o_wasan@hotmail.com>
2020-08-21 15:41:29 +0700

    update nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -22,8 +22,18 @@
 
                     </div>
                 @endauth
-            </div>
 
+            </div>
+            <div class="lg:ml-16">
+                <a href="https://www.facebook.com/moresmartstore" target="_blank"
+                class="text-blue-500 hover:text-blue-700">
+                    <svg class=" h-8 w-8 fill-current" viewBox="0 0 24 24">
+                    <path d="M17,1H3C1.9,1,1,1.9,1,3v14c0,1.101,0.9,2,2,2h7v-7H8V9.525h2V7.475c0-2.164,1.212-3.684,3.766-3.684l1.803,0.002v2.605
+        h-1.197C13.378,6.398,13,7.144,13,7.836v1.69h2.568L15,12h-2v7h4c1.1,0,2-0.899,2-2V3C19,1.9,18.1,1,17,1z"/>
+                    </svg>
+
+                </a>
+            </div>
             <div class="block lg:hidden ">
                 <button class="navbar-burger block  text-blue-500 hover:text-red-500 focus:text-gray-800 focus:outline-none">
                     <svg class="h-6 w-6 fill-current absolute" viewBox="0 0 24 24">
