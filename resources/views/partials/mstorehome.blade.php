60bd05c2969a7daae6c07cd93989d92d6958ce71
Wasan Ongart <o_wasan@hotmail.com>
2020-08-21 15:24:00 +0700

    update mstore

--- resources/views/partials/mstorehome.blade.php
+++ resources/views/partials/mstorehome.blade.php
@@ -14,14 +14,22 @@
             <p class="mt-2 leading-relaxed sm:text-base md:text-base  text-black  antialiased ">
                 แพลตฟอร์มออนไลน์ง่ายๆที่ให้ผู้ใช้โพสโปรโมทธุรกิจและแชร์ข้อมูลธุรกิจ โพสสินค้าและบริการได้ตามประเภทธุรกิจ เพื่อให้ผู้คนที่กำลังค้นหาข้อมูลธุรกิจ ได้รับข้อมูลที่ต้องการได้อย่างรวดเร็ว
             </p>
-            <div class=" mx-auto flex mt-4 justify-center ">
+            <div class=" mx-auto flex mt-4 justify-center  ">
                 <a href="/home"
-                    class="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 text-white bg-red-600 hover:bg-blue-600 text-base  leading-tight shadow-md">
+                    class="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 text-white bg-red-600 hover:bg-blue-600 text-sm  leading-tight shadow-md">
                     โปรโมทธุรกิจ ฟรี!
                 </a>
                 <a href="/pages"
-                    class="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 text-white bg-blue-500 hover:bg-red-600 text-base  leading-tight shadow-md">
+                    class="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 text-white bg-blue-500 hover:bg-red-600 text-sm  leading-tight shadow-md">
                     รู้จัก mStore
+                </a>
+                <a href="https://www.facebook.com/moresmartstore" target="_blank"
+                    class="ml-4 rounded-lg px-4 md:px-5 xl:px-4 py-3 md:py-4 xl:py-3 text-white bg-blue-500 hover:bg-blue-700 text-sm  leading-tight shadow-md">
+                        <svg class=" h-6 w-6 fill-current absolute" viewBox="0 0 24 24">
+                        <path d="M17,1H3C1.9,1,1,1.9,1,3v14c0,1.101,0.9,2,2,2h7v-7H8V9.525h2V7.475c0-2.164,1.212-3.684,3.766-3.684l1.803,0.002v2.605
+            h-1.197C13.378,6.398,13,7.144,13,7.836v1.69h2.568L15,12h-2v7h4c1.1,0,2-0.899,2-2V3C19,1.9,18.1,1,17,1z"/>
+                        </svg>
+                        <span class="ml-6">mStore</span>
                 </a>
             </div>
 
