0628f2ca0ae7c440e296cd3a13f27933d44194f7
Wasan Ongart <o_wasan@hotmail.com>
2020-09-28 15:36:57 +0700

    update column size

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -98,7 +98,7 @@
         {{-- //right-side --}}
 
         @if ( count($products) > 0 )
-             <div class="hidden  lg:block lg:w-2/3 h-full  ">
+             <div class="hidden  lg:block lg:w-1/3 h-full  ">
 
             @include('partials.headbar',[
                     'svg' => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" class="fill-current absolute"><path class="heroicon-ui" d="M5 3h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z"/></svg>',
