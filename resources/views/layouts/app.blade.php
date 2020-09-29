419d93653cc92a8faa9e63888eae7624e5bbd304
Wasan Ongart <o_wasan@hotmail.com>
2020-09-29 13:49:48 +0700

    update layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,7 +88,7 @@
         @yield('adsconversion')
 </head>
 
-<body class="font-sans h-auto bg-white  leading-normal tracking-normal antialiased">
+<body class="font-sans lg:h-full bg-white  leading-normal tracking-normal antialiased">
 
     <div>
 
