7c7a20afc8c51142ac8446832209392053c22c62
Wasan Ongart <o_wasan@hotmail.com>
2020-09-30 16:33:09 +0700

    update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -37,7 +37,7 @@
     <!--Container-->
     <div class="flex flex-wrap">
 
-        <nav id = "pages-nav" class="sm:block w-full hidden text-sm font-medium p-2  shadow-lg md:w-1/4 ">
+        <nav id = "pages-nav" class="sm:block w-full hidden text-sm font-medium shadow-lg md:w-1/4 ">
             <div class="hidden sm:block text-blue-700 border border-gray-200 p-2 items-center text-center text-base rounded-lg">
                 <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
                 <span class="ml-6">เกี่ยวกับ mStore</span>
@@ -45,7 +45,7 @@
             <div class="px-2 sm:flex sm:p-0 ">
                 <div class="w-full mx-auto m-1">
                     @foreach ($pages as $listpage)
-                        <a href="/pages/{{$listpage->slug}}" class="block p-2 text-sm  text-gray-900  rounded-full hover:bg-blue-500 hover:text-white">
+                        <a href="/pages/{{$listpage->slug}}" class="block text-sm text-gray-900 rounded hover:bg-blue-500 hover:text-white">
                             {{-- <svg xmlns="http://www.w3.org/2000/svg" class=" fill-current absolute" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M18.59 13H3a1 1 0 0 1 0-2h15.59l-5.3-5.3a1 1 0 1 1 1.42-1.4l7 7a1 1 0 0 1 0 1.4l-7 7a1 1 0 0 1-1.42-1.4l5.3-5.3z"/></svg> --}}
                             <span class="ml-6">{{$listpage->title}}</span>
                         </a>
