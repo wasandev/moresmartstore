cfd7de9d9894b9e00e3128e4856a710e83a46496
Wasan Ongart <o_wasan@hotmail.com>
2020-09-30 11:45:34 +0700

    final card view update

--- resources/views/posts/cardvendor.blade.php
+++ resources/views/posts/cardvendor.blade.php
@@ -18,12 +18,12 @@
                     </div>
 
                     <div class="w-full flex flex-row border-t border-gray-100 text-sm font-thin">
-                        <div class="text-left w-1/2 m-1">
+                        <div class="text-left w-1/3 m-1">
                             <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M17.56 17.66a8 8 0 0 1-11.32 0L1.3 12.7a1 1 0 0 1 0-1.42l4.95-4.95a8 8 0 0 1 11.32 0l4.95 4.95a1 1 0 0 1 0 1.42l-4.95 4.95zm-9.9-1.42a6 6 0 0 0 8.48 0L20.38 12l-4.24-4.24a6 6 0 0 0-8.48 0L3.4 12l4.25 4.24zM11.9 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0-2a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"/></svg>
                             <span class="pl-8 py-2">{{  $post->visits()->count() }}  </span>
                         </div>
 
-                        <div class="text-right w-1/2 mt-1">{{formatDateThai( $post->published_at )}}</div>
+                        <div class="text-right w-2/3 mt-1">{{formatDateThai( $post->published_at )}}</div>
                     </div>
                 </div>
             </a>
