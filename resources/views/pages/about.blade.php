d2f29246f9a77dbf5c8680e820dd7cefac6dffa3
Wasan Ongart <o_wasan@hotmail.com>
2020-09-30 17:00:10 +0700

    update data

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -42,10 +42,10 @@
                 <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/></svg>
                 <span class="ml-6">เกี่ยวกับ mStore</span>
             </div>
-            <div class="px-2 sm:flex sm:p-0 ">
+            <div class="py-2 sm:flex sm:p-0 ">
                 <div class="w-full mx-auto m-1">
                     @foreach ($pages as $listpage)
-                        <a href="/pages/{{$listpage->slug}}" class="block px-2 text-base font-normal text-gray-900 rounded hover:bg-blue-500 hover:text-white">
+                        <a href="/pages/{{$listpage->slug}}" class="block  text-base font-normal text-gray-900 rounded hover:bg-blue-500 hover:text-white">
                             {{-- <svg xmlns="http://www.w3.org/2000/svg" class=" fill-current absolute" viewBox="0 0 24 24" width="24" height="24"><path class="heroicon-ui" d="M18.59 13H3a1 1 0 0 1 0-2h15.59l-5.3-5.3a1 1 0 1 1 1.42-1.4l7 7a1 1 0 0 1 0 1.4l-7 7a1 1 0 0 1-1.42-1.4l5.3-5.3z"/></svg> --}}
                             <span class="ml-6">{{$listpage->title}}</span>
                         </a>
