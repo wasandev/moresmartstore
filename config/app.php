0c24c08ce0620c515f5ae0a3e7f9ed27b89b6858
Wasan Ongart <o_wasan@hotmail.com>
2021-04-26 12:43:14 +0700

    update nav check fb link on mobile

--- config/app.php
+++ config/app.php
@@ -175,7 +175,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\NovaServiceProvider::class,
-        Overtrue\LaravelFollow\FollowServiceProvider::class
+        Overtrue\LaravelFollow\FollowServiceProvider::class,
+        Jenssegers\Agent\AgentServiceProvider::class,
 
 
     ],
@@ -229,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Agent' => Jenssegers\Agent\Facades\Agent::class,
 
     ],
 
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -25,7 +25,8 @@
 
             </div>
             <div class="lg:ml-16">
-                <a href="fb://page/1434260050125109"
+                @if((new \Jenssegers\Agent\Agent())->isDesktop())
+                    <a href="https://www.facebook.com/moresmartstore" target="_blank"
                 class="text-blue-500 hover:text-blue-700">
                     <svg class=" h-8 w-8 fill-current" viewBox="0 0 24 24">
                     <path d="M17,1H3C1.9,1,1,1.9,1,3v14c0,1.101,0.9,2,2,2h7v-7H8V9.525h2V7.475c0-2.164,1.212-3.684,3.766-3.684l1.803,0.002v2.605
@@ -33,6 +34,33 @@
                     </svg>
 
                 </a>
+                @endif
+
+
+                @if((new \Jenssegers\Agent\Agent())->isMobile() || (new \Jenssegers\Agent\Agent())->isTablet() )
+                    @if((new \Jenssegers\Agent\Agent())->isAndroidOS())
+                                <a href="fb://page/1434260050125109"
+                                class="text-blue-500 hover:text-blue-700">
+                                    <svg class=" h-8 w-8 fill-current" viewBox="0 0 24 24">
+                                    <path d="M17,1H3C1.9,1,1,1.9,1,3v14c0,1.101,0.9,2,2,2h7v-7H8V9.525h2V7.475c0-2.164,1.212-3.684,3.766-3.684l1.803,0.002v2.605
+                        h-1.197C13.378,6.398,13,7.144,13,7.836v1.69h2.568L15,12h-2v7h4c1.1,0,2-0.899,2-2V3C19,1.9,18.1,1,17,1z"/>
+                                    </svg>
+
+                                </a>
+                    @endif
+                    @if((new \Jenssegers\Agent\Agent())->isiOS())
+                                <a href="fb://profile/1434260050125109"
+                                class="text-blue-500 hover:text-blue-700">
+                                    <svg class=" h-8 w-8 fill-current" viewBox="0 0 24 24">
+                                    <path d="M17,1H3C1.9,1,1,1.9,1,3v14c0,1.101,0.9,2,2,2h7v-7H8V9.525h2V7.475c0-2.164,1.212-3.684,3.766-3.684l1.803,0.002v2.605
+                        h-1.197C13.378,6.398,13,7.144,13,7.836v1.69h2.568L15,12h-2v7h4c1.1,0,2-0.899,2-2V3C19,1.9,18.1,1,17,1z"/>
+                                    </svg>
+
+                                </a>
+                    @endif
+
+                @endif
+
             </div>
             <div class="block lg:hidden ">
                 <button class="navbar-burger block  text-blue-500 hover:text-red-500 focus:text-gray-800 focus:outline-none">
