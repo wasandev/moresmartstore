58f0f59890ad8095ece538ce10f675c3b608e3de
Wasan Ongart <o_wasan@hotmail.com>
2021-05-13 13:29:07 +0700

    update dropdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,23 @@
         gtag('config', 'AW-625418009');
         </script>
         @yield('adsconversion')
-</head>
+        <script src="https://unpkg.com/@popperjs/core@2.9.1/dist/umd/popper.min.js" charset="utf-8"></script>
+        <script src="https://unpkg.com/@popperjs/core@2.9.1/dist/umd/popper.min.js" charset="utf-8"></script>
+<script>
+  function openDropdown(event,dropdownID){
+    let element = event.target;
+    while(element.nodeName !== "BUTTON"){
+      element = element.parentNode;
+    }
+    var popper = Popper.createPopper(element, document.getElementById(dropdownID), {
+      placement: 'top-end'
+    });
+    document.getElementById(dropdownID).classList.toggle("hidden");
+    document.getElementById(dropdownID).classList.toggle("block");
+  }
+</script>
+
+    </head>
 
 <body class="font-sans  bg-white  leading-normal tracking-normal antialiased">
 
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -17,7 +17,7 @@
 
                         </div>
                         <div id="notificationsMenu" class=" w-64 absolute  mt-0 p-2 bg-white rounded-lg shadow-xl invisible group-hover:visible">
-                            <p class="text-sm text-gray-700">ไม่มีข้อความแจ้งเตือน</p>
+                            <p class="text-base text-gray-700">ไม่มีข้อความแจ้งเตือน</p>
                         </div>
 
                     </div>
@@ -63,51 +63,80 @@
 
             </div>
             <div class="block lg:hidden ">
-                <button class="navbar-burger block  text-blue-500 hover:text-red-500 focus:text-gray-800 focus:outline-none">
+                <button class="block  text-blue-500 hover:text-red-500 focus:text-gray-800 focus:outline-none" type="button" onclick="openDropdown(event,'dropdown-id')">
                     <svg class="h-6 w-6 fill-current absolute" viewBox="0 0 24 24">
                     <path  fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
                     </svg>
                     <span class="ml-6">MENU</span>
                 </button>
+                 <div class="hidden bg-white  text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mb-1" style="min-width:12rem" id="dropdown-id">
+                    <a href="/" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    หน้าหลัก
+                    </a>
+                    <a href="/vendors" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    ข้อมูลธุรกิจ
+                    </a>
+                    <a href="/products" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    สินค้า/บริการ
+                    </a>
+                    <a href="/posts" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    โพส
+                    </a>
+                     <a href="/blogs" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    บทความ
+                    </a>
+                     <a href="/pages" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    About
+                    </a>
+                     @auth
+                        <div class="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25"></div>
+                            <a href="/home" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                            เพิ่มธุรกิจ
+                            </a>
+                        <div class="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25"></div>
+                        <a href="{{ route('logout') }}" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                            Logout
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                         @csrf
+                        </form>
+                    @else
+
+                     <div class="h-0 my-2 border border-solid border-t-0 border-blueGray-800 opacity-25"></div>
+                    <a href="{{ route('login') }}" class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700">
+                    Login
+                    </a>
+                    @endauth
+
+                </div>
             </div>
+
 
         </div>
 
 
 
-        <div id="main-nav" class="lg:block lg:w-auto hidden text-sm  text-left">
+        <div id="main-nav" class="lg:block lg:w-auto hidden text-base  text-left">
             <div class="lg:flex lg:justify-between items-center ">
                 <a href="/" class="mx-2 block px-3 py-1 text-gray-800 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M8 20H3V10H0L10 0l10 10h-3v10h-5v-6H8v6z"/>
-                    </svg> --}}
-                    <span>หน้าหลัก</span>
+                     <span>หน้าหลัก</span>
                 </a>
                 <a href="/vendors" class="mx-2 block px-3 py-1 text-gray-800 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100  ">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M2 2c0-1.1.9-2 2-2h12a2 2 0 0 1 2 2v18l-8-4-8 4V2z"/>
-                    </svg> --}}
-                    <span>ข้อมูลธุรกิจ</span>
+                     <span>ข้อมูลธุรกิจ</span>
                 </a>
                 <a href="/products" class="mx-2 block px-3 py-1 text-gray-800 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100 ">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path class="heroicon-ui" d="M5 3h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v4h4V5h-4zM5 13h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4H5zm10-2h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2h-4a2 2 0 0 1-2-2v-4c0-1.1.9-2 2-2zm0 2v4h4v-4h-4z"/>
-                    </svg> --}}
-                    <span>สินค้า/บริการ</span>
+                     <span>สินค้า/บริการ</span>
                 </a>
                 <a href="/post" class="mx-2 block px-3 py-1 text-gray-800 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100 ">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path class="heroicon-ui" d="M6.3 12.3l10-10a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.4l-10 10a1 1 0 0 1-.7.3H7a1 1 0 0 1-1-1v-4a1 1 0 0 1 .3-.7zM8 16h2.59l9-9L17 4.41l-9 9V16zm10-2a1 1 0 0 1 2 0v6a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2h6a1 1 0 0 1 0 2H4v14h14v-6z"/>
-                    </svg> --}}
-                    <span>โพส</span>
+                      <span>โพส</span>
                 </a>
                 <a href="/blogs" class="mx-2 block px-3 py-1 text-gray-800 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100  ">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M16 2h4v15a3 3 0 0 1-3 3H3a3 3 0 0 1-3-3V0h16v2zm0 2v13a1 1 0 0 0 1 1 1 1 0 0 0 1-1V4h-2zM2 2v15a1 1 0 0 0 1 1h11.17a2.98 2.98 0 0 1-.17-1V2H2zm2 8h8v2H4v-2zm0 4h8v2H4v-2zM4 4h8v4H4V4z"/>
-                    </svg> --}}
-                    <span>ข่าว/บทความ</span>
+                      <span>ข่าว/บทความ</span>
                 </a>
 
-                <a href="/pages" class="mx-2 px-3 py-1 block text-red-500 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100  ">
-                    {{-- <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path class="heroicon-ui" d="M12 22a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16zM10.59 8.59a1 1 0 1 1-1.42-1.42 4 4 0 1 1 5.66 5.66l-2.12 2.12a1 1 0 1 1-1.42-1.42l2.12-2.12A2 2 0 0 0 10.6 8.6zM12 18a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
-                    </svg> --}}
+                <a href="/pages" class="mx-2 px-3 py-1 block text-gray-800 mt-2 lg:mt-0  hover:bg-blue-500 hover:text-gray-100  ">
 
-                    <span class="font-semibold">รู้จัก mStore</span>
+                    <span class="font-semibold">About mStore</span>
                 </a>
                 @auth
 
@@ -123,11 +152,11 @@
                             <a href="/home"
                                 class="no-underline px-3 py-1 block text-grey-900 hover:text-blue-500">
 
-                                <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M18 9.87V20H2V9.87a4.25 4.25 0 0 0 3-.38V14h10V9.5a4.26 4.26 0 0 0 3 .37zM3 0h4l-.67 6.03A3.43 3.43 0 0 1 3 9C1.34 9 .42 7.73.95 6.15L3 0zm5 0h4l.7 6.3c.17 1.5-.91 2.7-2.42 2.7h-.56A2.38 2.38 0 0 1 7.3 6.3L8 0zm5 0h4l2.05 6.15C19.58 7.73 18.65 9 17 9a3.42 3.42 0 0 1-3.33-2.97L13 0z"/></svg>
                                 <span class="ml-6">โปรโมทธุรกิจ</span>
                             </a>
+                            {{--
                             <hr class="border-t mx-2 border-gray-300" >
-                            <a href="{{ url('/messages') }}" class="no-underline px-3 py-1 block text-grey-900 hover:text-blue-500">
+                             <a href="{{ url('/messages') }}" class="no-underline px-3 py-1 block text-grey-900 hover:text-blue-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M0 2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm14 12h4V2H2v12h4c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2z"/></svg>
                                 <span class="ml-6">กล่องข้อความ @include('messenger.unread-count')</span>
                             </a>
@@ -135,13 +164,12 @@
                             <a href="{{ url('/messages/create/1/แจ้งปัญหาการใช้งาน') }}" class="no-underline px-2 py-1 mt-2 block text-grey-900 hover:text-blue-500">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path class="heroicon-ui" d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 9a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0v4a1 1 0 0 1-1 1zm0 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg>
                                 <span class="ml-6">แจ้งปัญหาการใช้งาน</span>
-                            </a>
+                            </a> --}}
                             <hr class="border-t mx-2 border-gray-300">
 
                             <a href="{{ route('logout') }}"
                                 class=" no-underline px-3 py-1 block text-gray-900 hover:text-blue-500" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-                                  <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path d="M10 7H2v6h8v5l8-8-8-8v5z"/></svg>
 
                                   <span class="ml-6 text-red-500">{{ __('Logout') }}</span>
 
@@ -163,48 +191,7 @@
 
                 </div>
             </div>
-            @auth
-                <div class="px-3 py-1 lg:hidden bg-blue-200 mt-2">
-                    <div  class="w-full text-center">
-                        <img class="h-12 w-12 border-2 border-gray-600 rounded-full object-cover mx-auto" src="{{ Storage::url(Auth::user()->avatar) }}" alt="">
-                        <span class="text-base text-gray-800">{{ Auth::user()->name }}</span>
-                    </div>
-                    <div class="mt-4">
-                        @if (Auth::user()->role == 'member')
-                            <a href="/home" class="px-2 py-1 mt-2 block text-gray-800  rounded-full hover:bg-blue-500 hover:text-gray-100 lg:mt-0 lg:ml-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 20 20"><path d="M18 9.87V20H2V9.87a4.25 4.25 0 0 0 3-.38V14h10V9.5a4.26 4.26 0 0 0 3 .37zM3 0h4l-.67 6.03A3.43 3.43 0 0 1 3 9C1.34 9 .42 7.73.95 6.15L3 0zm5 0h4l.7 6.3c.17 1.5-.91 2.7-2.42 2.7h-.56A2.38 2.38 0 0 1 7.3 6.3L8 0zm5 0h4l2.05 6.15C19.58 7.73 18.65 9 17 9a3.42 3.42 0 0 1-3.33-2.97L13 0z"/></svg>
-                                <span class="ml-6">โปรโมทธุรกิจ</span>
-                            </a>
-                        @else
-                            <a href="/app" class="px-2 py-1 mt-2 block text-gray-800  rounded-full hover:bg-blue-500 hover:text-gray-100 lg:mt-0 lg:ml-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 20 20"><path d="M18 9.87V20H2V9.87a4.25 4.25 0 0 0 3-.38V14h10V9.5a4.26 4.26 0 0 0 3 .37zM3 0h4l-.67 6.03A3.43 3.43 0 0 1 3 9C1.34 9 .42 7.73.95 6.15L3 0zm5 0h4l.7 6.3c.17 1.5-.91 2.7-2.42 2.7h-.56A2.38 2.38 0 0 1 7.3 6.3L8 0zm5 0h4l2.05 6.15C19.58 7.73 18.65 9 17 9a3.42 3.42 0 0 1-3.33-2.97L13 0z"/></svg>
-                                <span class="ml-6">จัดการข้อมูลธุรกิจ</span>
-                            </a>
-                        @endif
-                        <hr class="border-t mx-2 border-gray-300">
-                        <a href="{{ url('/messages') }}" class="no-underline px-2 py-1 mt-2 block text-grey-900 hover:text-blue-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 20 20"><path d="M0 2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v16a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm14 12h4V2H2v12h4c0 1.1.9 2 2 2h4a2 2 0 0 0 2-2z"/></svg>
-                            <span class="ml-6">กล่องข้อความ @include('messenger.unread-count')</span>
-                        </a>
-                        <a href="{{ url('/messages/create/1/แจ้งปัญหาการใช้งาน') }}" class="no-underline px-2 py-1 mt-2 block text-grey-900 hover:text-blue-500">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 24 24"><path class="heroicon-ui" d="M12 2a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm0 9a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0v4a1 1 0 0 1-1 1zm0 4a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/></svg>
-                            <span class="ml-6">แจ้งปัญหาการใช้งาน</span>
-                        </a>
-                        <hr class="border-t mx-2 border-gray-300">
 
-                        <a href="{{ route('logout') }}" class="px-2 py-1 mt-2 block text-gray-800 rounded-full hover:bg-red-500 hover:text-gray-100 lg:mt-0 lg:ml-2"
-                            onclick="event.preventDefault();
-                            document.getElementById('logout-form').submit();">
-                             <svg xmlns="http://www.w3.org/2000/svg" class="fill-current absolute mr-2"  width="20" height="20" viewBox="0 0 20 20"><path d="M10 7H2v6h8v5l8-8-8-8v5z"/></svg>
-
-                             <span class="ml-6 ">{{ __('Logout') }}</span>
-                        </a>
-                        <form id="logout-form1" action="{{ route('logout') }}" method="POST" style="display: none;">
-                            @csrf
-                        </form>
-                    </div>
-                </div>
-            @endauth
         </div>
 </div>
 
